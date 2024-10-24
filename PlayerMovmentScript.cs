5dfea942822ba0ef1c6620e1977c9030b1bbe105
26florma <159549887+26florma@users.noreply.github.com>
2024-10-23 21:47:23 -0400

    Update PlayerMovmentScript.cs
    Test should contain variables for stem cells and damage also remember to add new scripts you make to your github you fool -_-

--- PlayerMovmentScript.cs
+++ PlayerMovmentScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerMomentScript : MonoBehaviour
 {
+    public int stemCells;
+    public int damage = 1;
+    public GameObject germs;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      germs = GameObject.FindGameObjectWithTag("germ")
     }
 
     // Update is called once per frame
